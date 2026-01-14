93d10d85c9ee82bf9db90a2bcb02519b610f3385
eaglelandsonce <66654533+eaglelandsonce@users.noreply.github.com>
2026-01-14 00:04:26 +0000

    lab9 fix

--- lab9/app.py
+++ lab9/app.py
@@ -22,4 +22,4 @@ def health():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))  # nosec B104 - Required for Docker container
